9381ee2c075f53c269a92da6a332f60fa37966b9
Denis14234 <den4ik14234@gmail.com>
2025-04-21 17:49:22 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Иван", "Сидоров", (byte) 33);
+        userService.saveUser("Николай", "Абромович", (byte) 45);
+        userService.saveUser("Анастасия", "Романова", (byte) 33);
+        userService.saveUser("Ольга", "Серябкина", (byte) 40);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
